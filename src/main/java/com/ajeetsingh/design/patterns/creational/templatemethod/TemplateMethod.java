13ba8eba27ac53221976d9ac3a563701cb57593b
Ajeet <ajeetsinghtomer@gmail.com>
2017-08-03 19:52:41 +0530

    java version moved to valriables

---
+++ src/main/java/com/ajeetsingh/design/patterns/creational/templatemethod/TemplateMethod.java
@@ -0,0 +1,11 @@
+package com.ajeetsingh.design.patterns.creational.templatemethod;
+
+import javax.xml.parsers.DocumentBuilder;
+import java.util.regex.Pattern;
+
+public final class TemplateMethod {
+
+    public static void main(String[] args) {
+
+    }
+}
